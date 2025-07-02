8d50c519e3193833e58977358676e6d1ec0ad0a1
princiamf2 <mmfuri6@gmail.com>
2025-07-02 14:51:19 +0200

    philo

--- philo_bonus/phylosopher.c
+++ philo_bonus/phylosopher.c
@@ -3,14 +3,15 @@
 /*                                                        :::      ::::::::   */
 /*   phylosopher.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mm-furi <mm-furi@student.42.fr>            +#+  +:+       +#+        */
+/*   By: michel <michel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/24 19:09:19 by mm-furi           #+#    #+#             */
-/*   Updated: 2025/06/26 18:15:00 by mm-furi          ###   ########.fr       */
+/*   Updated: 2025/07/02 14:43:50 by michel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "phylosopher_bonus.h"
+#include <unistd.h>
 
 int	eat_and_check(t_data2 *data, int id, long *last_meal, int *meals)
 {
@@ -39,35 +40,54 @@ void	do_sleep_and_think(t_data2 *data, int id)
 	safe_print(data, id, "is thinking");
 }
 
+
+static void	philo_init(t_data2 *data, int id, long *last_meal, int *meals)
+{
+    t_monitor *moni;
+    pthread_t watcher;
+
+    *last_meal = get_timestamp();
+    *meals     = 0;
+    moni = malloc(sizeof(*moni));
+    if (!moni)
+    {
+        perror("malloc");
+        return;
+    }
+    moni->data      = data;
+    moni->id        = id;
+    moni->last_meal = last_meal;
+    if (pthread_create(&watcher, NULL, death_monitor, moni) != 0)
+        perror("pthread_create");
+    pthread_detach(watcher);
+    if (id % 2 == 1)
+        usleep(data->args.time_to_eat * 1000);
+    else
+        usleep(data->args.time_to_sleep * 1000);
+}
+
+static void	philo_loop(t_data2 *data, int id, long *last_meal, int *meals)
+{
+    t_args2 *args = &data->args;
+
+    while (1)
+    {
+        take_forks(data);
+        safe_print(data, id, "has taken a fork");
+        safe_print(data, id, "has taken a fork");
+        eat_and_check(data, id, last_meal, meals);
+        drop_forks(data);
+        safe_print(data, id, "is sleeping");
+        usleep(args->time_to_sleep * 1000);
+        safe_print(data, id, "is thinking");
+    }
+}
+
 void	routine_philo(t_data2 *data, int id)
 {
-	long		last_meal;
-	int			meals;
-	t_args2		*args;
-	pthread_t	watcher;
-	t_monitor	*moni;
+    long last_meal;
+    int  meals;
 
-	args = &data->args;
-	last_meal = get_timestamp();
-	meals = 0;
-	moni = malloc(sizeof(*moni));
-	moni->data = data;
-	moni->id = id;
-	moni->last_meal = &last_meal;
-	if (pthread_create(&watcher, NULL, death_monitor, moni) != 0)
-		perror("pthread_create");
-	pthread_detach(watcher);
-	if (id % 2 == 1)
-		usleep(data->args.time_to_eat * 1000);
-	while (1)
-	{
-		take_forks(data);
-		safe_print(data, id, "has taken a fork");
-		safe_print(data, id, "has taken a fork");
-		eat_and_check(data, id, &last_meal, &meals);
-		drop_forks(data);
-		safe_print(data, id, "is sleeping");
-		usleep(args->time_to_sleep * 1000);
-		safe_print(data, id, "is thinking");
-	}
+    philo_init(data, id, &last_meal, &meals);
+    philo_loop(data, id, &last_meal, &meals);
 }
